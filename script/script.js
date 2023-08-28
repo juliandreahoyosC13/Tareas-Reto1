2d7728ce41b5a2dce67f5753add390d4235e049d
JR <javierruizarango@gmail.com>
2023-08-28 09:16:25 -0500

    agregando el codigo sugerido para el dom 5 que esta en el redme de github

--- script/script.js
+++ script/script.js
@@ -35,6 +35,18 @@ function displayFooter() {
 
 // Codigo DOM #5
 
+// Permite que la acción guardar el nuevo nombre de la tarea cuando decides editar y que impacte el DOM del HTML, acá debes agegar algoritmo de actualizar tarea
+
+function activateSaveListeners() {
+  const saveBtn = document.querySelectorAll('.saveBtn')
+  const inputs = document.querySelectorAll('.input-controller textarea')
+  saveBtn.forEach((sB, i) => {
+    sB.addEventListener('click', () => {
+      // Llamar la función que guarda la actualización la tarea
+    })
+  })
+}
+
 // Codigo DOM #6
 
 //El sistema debe permitir EDITAR o MODIFICAR una tarea.
